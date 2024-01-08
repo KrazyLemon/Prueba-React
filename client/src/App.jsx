d9198da635885bc313bd326348ab62b9314720d8
KrazyLemon <angeleduvelazquez@gmail.com>
2024-01-08 12:45:38 -0600

    Primer Commit Pueba tecnica frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import NavBar from './components/NavBar'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  );
+}
+
+export default App
